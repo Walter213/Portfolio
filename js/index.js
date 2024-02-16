acb7ee05135894db969f615722cfaa530f90ce5a
Walter213 <46505629+Walter213@users.noreply.github.com>
2024-02-16 16:28:42 -0700

    Navigation Bar rework and fixing skills
    Fixed skills image on showing properly and reworked the entire Navigation bar

--- js/index.js
+++ js/index.js
@@ -1,42 +1,42 @@
 'use strict';
  
  
-window.addEventListener("load", function(event) {
-   let menuOpen=false;
-   const mobileMenuButton = document.querySelector('#mobile-menu-button');
-   const menuLinks = document.querySelectorAll('.mobile-link');
-   const menuPanel = document.querySelector('#menu-panel');
+// window.addEventListener("load", function(event) {
+//    let menuOpen=false;
+//    const mobileMenuButton = document.querySelector('#mobile-menu-button');
+//    const menuLinks = document.querySelectorAll('#mobile-link-small');
+//    const menuPanel = document.querySelector('#mobile-links-small-view');
  
- Array.from(menuLinks).forEach((item)=>{
-   item.addEventListener('click', function( ){
-    menuPanel.removeEventListener("mouseleave", onLeavePanel)
-    closeMobileMenu();
-   })
- })
+//  Array.from(menuLinks).forEach((item)=>{
+//    item.addEventListener('click', function( ){
+//     menuPanel.removeEventListener("mouseleave", onLeavePanel)
+//     closeMobileMenu();
+//    })
+//  })
  
  
- mobileMenuButton.onclick = (e)=>{
-  menuOpen?closeMobileMenu():openMobileMenu();
+//  mobileMenuButton.onclick = (e)=>{
+//   menuOpen?closeMobileMenu():openMobileMenu();
   
-}
-function onLeavePanel(){
-  closeMobileMenu()
-}
-const openMobileMenu=(e)=>{
-  menuPanel.addEventListener("mouseleave", onLeavePanel)
-   TweenLite.to(menuPanel, 0.4, {top:50, ease: Power1.easeOut});
-   menuOpen = true;
-}
+// }
+// function onLeavePanel(){
+//   closeMobileMenu()
+// }
+// const openMobileMenu=(e)=>{
+//   menuPanel.addEventListener("mouseleave", onLeavePanel)
+//    TweenLite.to(menuPanel, 0.4, {right:50, ease: Power1.easeOut});
+//    menuOpen = true;
+// }
  
-const closeMobileMenu=(e)=>{
-  menuPanel.removeEventListener("mouseleave", onLeavePanel)
-  TweenLite.to(menuPanel, 0.4, 
-    {
-      top:-menuPanel.clientHeight,
-      ease: Power1.easeOut,
-    }
-)
-  menuOpen =false;
-}
-});
+// const closeMobileMenu=(e)=>{
+//   menuPanel.removeEventListener("mouseleave", onLeavePanel)
+//   TweenLite.to(menuPanel, 0.4, 
+//     {
+//       top:-menuPanel.clientLeft,
+//       ease: Power1.easeOut,
+//     }
+// )
+//   menuOpen =false;
+// }
+// });
  
